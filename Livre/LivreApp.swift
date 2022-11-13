8a3dfe8c4e64db0306dfc61413b93a8aa54fea50
Louis Rey <louisreypro@yahoo.com>
2022-11-13 16:23:22 +0100

    Initial Commit

---
+++ Livre/LivreApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivreApp.swift
+//  Livre
+//
+//  Created by Louis REY on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LivreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
